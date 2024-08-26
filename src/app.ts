1181eadfad9dcbfd501bdd2c4a5fac6946876be3
Nilesh kasar <128775584+nilkasar929@users.noreply.github.com>
2024-08-26 12:09:55 +0530

    minor changes

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,12 @@ app.get('/',(req:Request,res:Response)=>{
     res.send("Hello there");
 });
 
+app.post('/login',(req:Request,res:Response)=>{
+    const {username,password} = req.body;
+    res.send(`Hello ${username}`);
+
+})
+
 app.listen(PORT,()=>{
     console.log(`server is running on port ${PORT}`)
 })
